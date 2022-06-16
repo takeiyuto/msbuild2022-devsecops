bf848b2c4338123f6094905a9a22b63ed4a493d1
Yuto Takei <37465047+takeiyuto@users.noreply.github.com>
2022-06-16 10:03:27 +0900

    Added Storage account key

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const config = {
     user: "dbuser",
 };
 
+const key = "DefaultEndpointsProtocol=https;AccountName=demo20220616;AccountKey=om0sy22Aq6PppgyfYhOBkefTbQ69vkzd5PJBZVPQRcjT6Hq1VWFpA3rTArcRvPRupqcSJfDsawcP+ASt82On5Q==;EndpointSuffix=core.windows.net";
+
 app.get("/", async (req, res) => {
     await sql.connect(config);
     await sql.query("select * from users where id = " + req.params.id);
